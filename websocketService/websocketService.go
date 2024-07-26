e55f20a74e65dcfb1cce6fab34b1abacf68fafed
Metalxiaoxiao <2735466890@qq.com>
2024-07-27 07:47:21 +0800

    FIX

--- websocketService/websocketService.go
+++ websocketService/websocketService.go
@@ -184,7 +184,7 @@ func HandleWebSocket(w http.ResponseWriter, r *http.Request) {
 					for _, friendId := range friends {
 						if *Clients[userID].UserState != jsonprovider.Stealth {
 							sendJSONToUserWithRlock(friendId, jsonprovider.UserStateEvent{
-								UserID:    friendId,
+								UserID:    userID,
 								UserState: userState,
 							}, configData.Commands.UserStateEvent)
 						}
